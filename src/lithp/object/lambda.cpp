0dde084a42cf7a363d2c05cf010951307c025433
Frederik Gahr <10232152+fgahr@users.noreply.github.com>
2020-02-10 10:01:59 +0100

    Move implementations of Reference methods into namespace.

--- src/lithp/object/lambda.cpp
+++ src/lithp/object/lambda.cpp
@@ -4,38 +4,28 @@ namespace lithp {
 namespace {
 class Reference : public Object {
 public:
-  Reference(Object **obj);
+  Reference(Object **obj) : ref{obj} {}
   virtual ~Reference() override = default;
   virtual size_t size() override { return sizeof(Reference); }
   virtual Type type() override { return Type::Reference; }
-  virtual Object *eval(Environment &env) override;
-  virtual void repr(std::ostream &out) override;
-  virtual RefStream refs() override;
-  virtual Object *copy_to(void *mem) override;
+  virtual Object *eval(Environment &env) override {
+    // We can assume that references need not be evaluated here:
+    // Either they point to function slots whose contents are evaluated
+    // beforehand, or they point to variables which are captured from outside.
+    return *ref;
+  }
+  virtual void repr(std::ostream &out) override {
+    throw std::logic_error{"attempting to print a Reference"};
+  }
+  virtual RefStream refs() override { return RefStream::of({ref}); }
+  virtual Object *copy_to(void *mem) override {
+    throw std::logic_error{"attempting to copy a Reference"};
+  }
 
 private:
   Object **ref;
 };
 } // namespace
-
-Reference::Reference(Object **obj) : ref{obj} {}
-
-Object *Reference::eval(Environment &env) {
-  // We can assume that references need not be evaluated here:
-  // Either they point to function slots whose contents are evaluated
-  // beforehand, or they point to variables which are captured from outside.
-  return *ref;
-}
-
-void Reference::repr(std::ostream &out) {
-  throw std::logic_error{"attempting to print a Reference"};
-}
-
-RefStream Reference::refs() { return RefStream::of({ref}); }
-
-Object *Reference::copy_to(void *mem) {
-  throw std::logic_error{"attempting to copy a Reference"};
-}
 
 Lambda *Lambda::of(std::vector<Symbol *> args, Symbol *rest, Object *body) {
   if (args.size() > MAX_NUM_ARGS) {
