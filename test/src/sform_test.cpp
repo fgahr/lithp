60a4730de19410be718642a758337055d2b8e1e0
Frederik Gahr <10232152+fgahr@users.noreply.github.com>
2020-03-07 22:39:52 +0100

    Add tests for `if` and `quote`.

--- test/src/sform_test.cpp
+++ test/src/sform_test.cpp
@@ -8,3 +8,19 @@ TEST_F(RuntimeTest, special_forms_exist) {
   EXPECT_TRUE(SpecialForm::exists(SYM("quote")));
   EXPECT_TRUE(SpecialForm::exists(SYM("set!")));
 }
+
+TEST_F(RuntimeTest, if_works) {
+  EXPECT_EQ(eval(List::of({SYM("if"), SYM("true"), NUM(0), NUM(1)}), ENV),
+            NUM(0));
+  EXPECT_EQ(eval(List::of({SYM("if"), SYM("false"), NUM(0), NUM(1)}), ENV),
+            NUM(1));
+  EXPECT_EQ(eval(List::of({SYM("if"), SYM("false"), NUM(0)}), ENV), nil());
+  EXPECT_EQ(eval(List::of({SYM("if"), SYM("true"), NUM(0)}), ENV), NUM(0));
+}
+
+TEST_F(RuntimeTest, quote_works) {
+  EXPECT_EQ(eval(QUOTE(SYM("foo")), ENV), SYM("foo"));
+  EXPECT_EQ(eval(QUOTE(NUM(20)), ENV), NUM(20));
+  List *list = List::of({SYM("foo"), SYM("bar"), NUM(8)});
+  EXPECT_EQ(eval(QUOTE(list), ENV), list);
+}
