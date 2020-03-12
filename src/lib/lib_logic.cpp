cd3d668ca5770e0936ef927a4b4049bb51f12c7e
Frederik Gahr <10232152+fgahr@users.noreply.github.com>
2020-03-12 18:14:49 +0100

    Add logic library to stdlib.

---
+++ src/lib/lib_logic.cpp
@@ -0,0 +1,29 @@
+#include <lithp.hpp>
+
+#include "lib_logic.hpp"
+#include "lib_util.hpp"
+
+namespace lithp::lib {
+namespace logic {
+Object *not_(size_t, Object **args) {
+  if (is_true(args[0])) {
+    return Boolean::False();
+  } else {
+    return Boolean::True();
+  }
+}
+
+Object *true_(size_t, Object **args) {
+  return Boolean::of(args[0] == Boolean::True());
+}
+
+Object *false_(size_t, Object **args) {
+  return Boolean::of(args[0] == Boolean::False());
+}
+} // namespace logic
+void load_logic(Environment &env) {
+  env.def(SYM("not"), FUN(1, false, logic::not_));
+  env.def(SYM("true?"), FUN(1, false, logic::true_));
+  env.def(SYM("false?"), FUN(1, false, logic::false_));
+}
+} // namespace lithp::lib
--- src/lib/stdlib.cpp
+++ src/lib/stdlib.cpp
@@ -2,11 +2,13 @@
 #include <lithp.hpp>
 
 #include "lib_data.hpp"
+#include "lib_logic.hpp"
 #include "lib_numeric.hpp"
 
 namespace lithp::lib {
 void load_stdlib(Environment &env) {
-    load_data(env);
-    load_numeric(env);
+  load_data(env);
+  load_numeric(env);
+  load_logic(env);
 }
 } // namespace lithp::lib
